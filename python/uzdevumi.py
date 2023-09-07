c58bf2879f35335238ae9ede47840d089d5d4c70
Skola <martabrita@gmail.com>
2023-09-07 14:22:42 +0300

    stundas kopkods

--- python/uzdevumi.py
+++ python/uzdevumi.py
@@ -2,7 +2,7 @@ def tests(parametrs):
     a = parametrs #darbības ar parametriem
     return a #funkcijas rezultāts
 
-print(tests("kaķis"))
+# print(tests("kaķis"))
 
 def pirmais(par1, par2):
     reizinajums = par1*par2
@@ -12,4 +12,43 @@ def pirmais(par1, par2):
     else:
         return summa
 
-print("The result is", pirmais(20,30) )
+# print("The result is", pirmais(20,30) )
+
+def otrais(): #def otrais(sakums = 0, beigas = 10, solis = 1)
+    esosais = 0
+    ieprieksejais = 0
+    for i in range(10): #range(sakums, beigas, solis)
+        ieprieksejais = esosais
+        esosais = i
+        summa = ieprieksejais+esosais
+        print("Current Number", esosais, "Previous Number", ieprieksejais, "Sum:", summa)
+    return
+
+# otrais()
+
+def tresais(teksts):
+    print("Sākotnējais teksts ir ", teksts)
+    print("Tikai pāra indeksa burti:")
+    for i in range(0, len(teksts), 2):
+        print(teksts[i])
+    return
+
+#tresais("pynative")
+
+def ceturtais(teksts, n):
+    print("Teksts:", teksts)
+    print("Noņemot pirmos",n,"burtus sanāk:",teksts[n:])
+    return
+
+# ceturtais("pynative",4)
+
+def piektais(saraksts):
+    print("Dotais saraksts:", saraksts)
+    print("Result is", saraksts[0]==saraksts[-1])
+    return
+
+skaitli1 = [10, 20, 30, 40, 10]
+skaitli2 = [75, 65, 35, 75, 30]
+
+piektais(skaitli2)
+
