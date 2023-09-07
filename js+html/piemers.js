8f23700af42b7233fed518706d6698b99750a348
Skola <martabrita@gmail.com>
2023-09-07 13:21:21 +0300

    pirmais commit

---
+++ js+html/piemers.js
@@ -0,0 +1,7 @@
+function tests(parametrs){
+    let a = parametrs //darbības, kuras jādara funkcijai
+    return a //funkcijas rezultāts
+}
+
+
+console.log(tests("kaķis"))
---
+++ python/uzdevumi.py
@@ -0,0 +1,15 @@
+def tests(parametrs):
+    a = parametrs #darbības ar parametriem
+    return a #funkcijas rezultāts
+
+print(tests("kaķis"))
+
+def pirmais(par1, par2):
+    reizinajums = par1*par2
+    summa = par1+par2
+    if reizinajums<1000 :
+        return reizinajums
+    else:
+        return summa
+
+print("The result is", pirmais(40,30) )
